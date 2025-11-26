238989c52cb3253e63645d6aef68efb45d8ab161
velmurugan <velmurugan61682@gmail.com>
2025-11-26 23:30:31 +0530

    day3

---
+++ Notes/day3.js
@@ -0,0 +1,84 @@
+
+    // 1qestion
+//     function vel(){
+//         console.log("print the series of 1234 ...8 using for loop");
+//         for (let i=1;i<=8;i++)
+//     {
+//     console. log(i);
+//     }}
+//     vel()
+//     function vel(n){
+//     console.log("print the series of 1234...8 while loop");
+//     let j=1;
+//     while (j<=n)
+//     {
+//         console.log (j);
+//         j++;
+//       }}
+//       vel(8)
+// function vel(n){
+//       console.log("print the series of 1234...8 do loop");
+//       let j=1;
+//       do
+//       {
+//        console.log(n);
+//           let num="";
+//            n++;
+//       }
+//       while(j<n);
+//       return (num)
+// }
+// vel(10);
+    //   2 question
+// console.log("print the series of 1357 series odd using for loop");
+// for (  i=1;i<=9;i+=2)
+// {
+// console.log(i)
+
+// }
+//  console.log("print the series of 13579 using while loop");
+//      let j=1;
+//     while (j<=9)
+//     {
+//         console.log (j);
+//         j+=2;
+//     }
+    
+//       console.log("print the series of 13579 using do loop");
+//       let k=1;
+//       do
+//       {
+//          console.log(k);
+//          k+=2;
+//       }
+//           while(k<=9);
+        // 3question
+     console.log("print the series of 02468  using for loop");
+       for(let i=0;i<=10;i++)
+      {
+      if(i%2===0)
+         console.log(i);
+
+      }
+ console.log("print the series of 02468using while loop");
+     let j=0;
+    while (j<=10)
+    {
+      if(j%2===0)
+        console.log (j);
+        j++;
+    }
+    
+      console.log("print the series of 13579 using do loop");
+      let k=0;
+      do
+      {if(k%2===0)
+         console.log(k);
+         k++;
+      }
+          while(k<=10);
+
+
+
+
+
