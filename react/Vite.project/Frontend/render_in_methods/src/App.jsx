8b89ddd49ee131c1fe569b9eb0046a6dce444142
velmurugan <velmurugan61682@gmail.com>
2025-12-09 19:49:18 +0530

    UPDATE

--- react/Vite.project/Frontend/render_in_methods/src/App.jsx
+++ react/Vite.project/Frontend/render_in_methods/src/App.jsx
@@ -6,8 +6,8 @@ import Trafici from "./components/Traffic"
 import Nulls from "./components/Nulls"
 import Traffic from "./components/Traffic"
 import Movies from "./components/Movies"
-import Resume from "./components/Resume"
-import Leo from "./components/Leo"
+import Resume from "./12DAY/Resume"
+import Leo from "./12DAY/Leo"
 
 
 
